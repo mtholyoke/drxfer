47996496b0b41e52c197abf4cb841047d4f7ad45
Sven Aas <sven.aas@gmail.com>
2011-08-29 21:31:19 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drxfer::Application.config.secret_token = '1b53e9e90ceb8375fa987f3f186a6f886b8e4644ef5b8a1e1c363c0ae3f7412160e67a9238ec6f5a06eb8ac54f8be81262aea8f13fa28fcf207d80fa7ceddfe1'
