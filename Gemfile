2ea12a62d751287b322e7f9c27faa79e2b523501
Sven Aas <sven.aas@gmail.com>
2013-02-03 15:32:25 -0500

    Updated Rails version to 3.1.10, with associated updates to other gems.

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.3'
-gem 'rake', '0.8.7'
+gem 'rails', '3.1.10'
+gem 'rake', '10.0.3'
 
-gem "devise", "~> 1.4"
+gem "devise", "~> 2.2.3"
 gem "devise_ldap_authenticatable"
 
 gem 'carrierwave'
@@ -12,7 +12,7 @@ gem 'capistrano'
 
 gem 'exception_notification'
 
-gem 'nested_form', :git => "git://github.com/ryanb/nested_form.git" 
+gem 'nested_form', :git => "git://github.com/ryanb/nested_form.git"
 # (See https://github.com/ryanb/nested_form/issues/95#issuecomment-2187551)
 
 group :development, :test do
@@ -29,8 +29,8 @@ end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails', " ~> 3.1.0"
-  gem 'coffee-rails', " ~> 3.1.0"
+  gem 'sass-rails', " ~> 3.1.7"
+  gem 'coffee-rails', " ~> 3.1.1"
   gem 'uglifier'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,27 +4,27 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :password, :email, :first_name, :last_name, :admin
-  
+
   has_many :assignments
   has_many :transfers
   has_many :agreements, :through => :assignments
-  
+
   before_save :get_ldap_data
-  
+
   default_scope :order => 'admin DESC, last_name ASC, first_name ASC'
-  
+
   def get_ldap_data
     return if Rails.env == 'test'
     begin
-      self.email      = Devise::LdapAdapter.get_ldap_param(self.username,"mail")[0]
-      self.first_name = Devise::LdapAdapter.get_ldap_param(self.username,"givenname")[0]
-      self.last_name  = Devise::LdapAdapter.get_ldap_param(self.username,"sn")[0]
+      self.email      = Devise::LdapAdapter.get_ldap_param(self.username,"mail")
+      self.first_name = Devise::LdapAdapter.get_ldap_param(self.username,"givenname")
+      self.last_name  = Devise::LdapAdapter.get_ldap_param(self.username,"sn")
     rescue
       self.errors[:username] ="#{self.username} not found"
       return false
     end
   end
-  
+
   def full_name
     first_name + ' ' + last_name
   end
