83b0c28661d5f17dcd5f926167794135e5510c3b
Lydia Cheah <cheahlizuan@gmail.com>
2017-02-24 11:15:06 -0500

    add tests for agreement and assignment models

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Assignment do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context "associations" do
+  		it { should belong_to(:user) }
+  		it { should belong_to(:agreement) }
+  	end
 end
