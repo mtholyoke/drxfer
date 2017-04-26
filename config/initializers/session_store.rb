497016c5aaaca32db22bca7f42a181f3788ae915
Lydia Li Zuan Cheah <cheahlizuan@gmail.com>
2017-04-26 15:42:16 -0400

    Update and add necessary files for Rails 4.1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Drxfer::Application.config.session_store :cookie_store, :key => '_drxfer_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Drxfer::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_drxfer_session'
