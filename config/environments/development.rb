4713e75726fd0e9ba5cedce2f843cb0c8e0ce283
Jason Proctor <jproctor@mtholyoke.edu>
2017-09-20 13:30:54 -0400

    Enable console log for VM environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Enable the console in a VM-based dev environment
+  if Rails.env.development? && ENV.key?('VM_HOST_ADDRESS')
+    config.web_console.whitelisted_ips = ENV['VM_HOST_ADDRESS']
+  end
 end
