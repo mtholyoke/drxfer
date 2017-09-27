6c58345eeab3997234a5d1199634ca2d6f3a8d0e
Jason Proctor <jproctor@mtholyoke.edu>
2017-09-27 18:09:38 -0400

    More modernization

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,23 @@ class User < ActiveRecord::Base
   default_scope  { order(:admin => :desc, :last_name => :asc, :first_name => :asc) }
 
   def get_ldap_data
-   return if Rails.env.test? || Rails.env.development? 
+   return if Rails.env.test? || Rails.env.development?
     begin
       self.email      = Devise::LdapAdapter.get_ldap_param(self.username,"mail")
+    rescue
+      self.errors.add(:email, "#{self.username} email not found")
+      throw(:abort)
+    end
+    begin
       self.first_name = Devise::LdapAdapter.get_ldap_param(self.username,"givenname")
+    rescue
+      self.errors.add(:first_name, "#{self.username} first_name not found")
+      throw(:abort)
+    end
+    begin
       self.last_name  = Devise::LdapAdapter.get_ldap_param(self.username,"sn")
     rescue
-      self.errors[:username] ="#{self.username} not found"
+      self.errors.add(:last_name, "#{self.username} last_name not found")
       throw(:abort)
     end
   end
