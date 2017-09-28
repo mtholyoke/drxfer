d4e3c92588ed41074a6aa1f006ffa8da32e77679
Lydia Li Zuan Cheah <cheahlizuan@gmail.com>
2017-09-28 16:27:59 -0400

    Override Devise after_sign_in_path_for to return root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,10 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
+  
+  def after_sign_in_path_for(resource)
+    root_path  
+  end
 
   protected
 
