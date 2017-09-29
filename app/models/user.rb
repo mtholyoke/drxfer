82f8a02ca0c912b6c182f31d9f44ece7a6eb29f6
Jason Proctor <jproctor@mtholyoke.edu>
2017-09-29 15:06:42 -0400

    Leave a TODO to remind us to revisit getting user data from auth directory

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,40 +7,23 @@ class User < ActiveRecord::Base
   has_many :agreements, :through => :assignments
 
   # before_save :get_ldap_data
-  before_save :check_email
 
   default_scope  { order(:admin => :desc, :last_name => :asc, :first_name => :asc) }
 
+  # TODO: figure out why these were nil after LDAP server switch
   # def get_ldap_data
-  #  return if Rails.env.test? || Rails.env.development?
+  #   return if Rails.env.test? || Rails.env.development?
   #   begin
-  #     self.email      = Devise::LDAP::Adapter.get_ldap_param(self.username,"mail").first
-  #   rescue
-  #     self.errors.add(:email, "#{self.username} email not found")
-  #     throw(:abort)
-  #   end
-  #   begin
+  #     self.email      = Devise::LDAP::Adapter.get_ldap_param(self.username, "mail").first
   #     self.first_name = Devise::LDAP::Adapter.get_ldap_param(self.username,"givenname").first
-  #   rescue
-  #     self.errors.add(:first_name, "#{self.username} first_name not found")
-  #     throw(:abort)
-  #   end
-  #   begin
   #     self.last_name  = Devise::LDAP::Adapter.get_ldap_param(self.username,"sn").first
   #   rescue
-  #     self.errors.add(:last_name, "#{self.username} last_name not found")
+  #     self.errors.add(:username, "#{self.username} missing LDAP parameter(s)")
   #     throw(:abort)
   #   end
   # end
 
   def full_name
     first_name + ' ' + last_name
-  end
-
-  def check_email
-    unless self.email.include? "@mtholyoke.edu"
-      self.errors.add(:email, "is not valid.")
-      throw(:abort)
-    end
   end
 end
