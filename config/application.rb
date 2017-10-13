484b38afea1857e02803b21a0f1af8d76648092f
Jason Proctor <jproctor@mtholyoke.edu>
2017-10-13 10:33:11 -0400

    Refactor external assets to allow lists, move to assets.rb initializer

--- config/application.rb
+++ config/application.rb
@@ -11,21 +11,11 @@ module Drxfer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Create environment variables for external assets' sources if config/external_assets.yml exists
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'external_assets.yml')
-      if File.exists?(env_file) 
-          YAML.load(File.open(env_file)).each do |key, value|
-            ENV[key.to_s] = value
-          end
-      end
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set base path for transfered files. All files will be copied into this path.            
+    # Set base path for transfered files. All files will be copied into this path.
     # *** MAKE SURE *** to keep the base path description in sync so that it is described correctly.
     config.transfer_destination_base_path = Rails.root
     config.transfer_destination_base_path_description = "the application's root folder."
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,21 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Create config variable for external assets' sources if config/external_assets.yml exists
+external_assets_file = File.join(Rails.root, "config", "external_assets.yml")
+if File.exists? external_assets_file
+  Rails.application.config.external_assets = Hash.new
+  external_assets_data = YAML.load(File.open external_assets_file)
+  if external_assets_data
+    external_assets_data.each do |key, value|
+      if key == Rails.env
+        value.each do |env_key, env_value|
+          Rails.application.config.external_assets[env_key] = env_value
+        end
+      else
+        Rails.application.config.external_assets[key] = value
+      end
+    end
+  end
+end
